4ab198e50049e3618907052875bc308e2f41dffe
Guillaume Huysmans <ghuysmans99@gmail.com>
2017-10-02 01:06:00 +0200

    Implemented closures!

--- value.ml
+++ value.ml
@@ -6,6 +6,7 @@ type t =
   | Lit of int (* literal *)
   | Abs of id * t (* abstraction (function) *)
   | App of t * t (* application *)
+  | Closure of env * t (* partial application *)
   | Native of (env -> t) (* native function *)
 and env =
   (id * t) list
@@ -20,13 +21,15 @@ let get env id =
 let rec string_of_env env =
   env |>
   List.map (fun (id, e) -> id ^ " = " ^ string_of_t e) |>
-  String.concat ", "
+  String.concat ", " |> fun l ->
+  "[" ^ l ^ "]"
 and string_of_t = function
   | Id id -> id
   | Lit lit -> string_of_int lit
-  | Native _ -> "<native>"
   | Abs (id, e) -> "(\\" ^ id ^ ". " ^ string_of_t e ^ ")"
   | App (f, e) -> "(" ^ string_of_t f ^ " " ^ string_of_t e ^ ")"
+  | Closure (e, x) -> "(" ^ string_of_t x ^ ") " ^ string_of_env e
+  | Native _ -> "<native>"
 
 let type_error x expected =
   failwith @@ "Type error: `" ^ string_of_t x ^ "` is not " ^ expected ^ "."
@@ -46,20 +49,30 @@ let rec eval env = function
   | Lit _ as lit ->
     (* fully evaluated *)
     lit
-  | Native f as n ->
-    (* hackish *)
-    f env
   | Abs _ as a ->
     (* we can't do anything yet *)
     a
-  | App (f, e) ->
+  | Native f ->
+    (* hackish *)
+    f env
+  | Closure (_, Abs _) as c ->
+    (* not completely applied yet, so let's leave it alone *)
+    c
+  | Closure (env, e) ->
+    (* "reduce" other closures *)
+    eval env e
+  | App (f, param) ->
+    let f = eval env f in (* avoid identifiers... *)
+    let param = eval env param in (* evaluate the argument first *)
     match f with
-    | Id id ->
-      (* FIXME I don't think it always terminates... *)
-      eval env (App (get env id, e))
-    | Abs (id, b) ->
-      let param = eval env e in
+    | Abs (id, e) ->
+      (* create a closure *)
       let env' = (id, param) :: env in
-      eval env' b
+      eval env' (Closure (env', e))
+    | Closure (env, Abs (id, e)) ->
+      (* extend an existing closure *)
+      let env' = (id, param) :: env in
+      eval env' (Closure (env', e))
     | x ->
+      (* must be an error *)
       type_error x "a function"
